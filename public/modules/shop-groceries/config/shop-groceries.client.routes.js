e0093de69dd6ed9463e8b1a3b63b06a5bd8ffd5d
gehlotchirag <gehlotchirag1988@gmail.com>
2015-11-25 10:14:45 +0530

    listnear page added

--- public/modules/shop-groceries/config/shop-groceries.client.routes.js
+++ public/modules/shop-groceries/config/shop-groceries.client.routes.js
@@ -2,24 +2,28 @@
 
 //Setting up route
 angular.module('shop-groceries').config(['$stateProvider',
-	function($stateProvider) {
-		// Shop groceries state routing
-		$stateProvider.
-		state('listShopGroceries', {
-			url: '/shop-groceries',
-			templateUrl: 'modules/shop-groceries/views/list-shop-groceries.client.view.html'
-		}).
-		state('createShopGrocery', {
-			url: '/shop-groceries/create',
-			templateUrl: 'modules/shop-groceries/views/create-shop-grocery.client.view.html'
-		}).
-		state('viewShopGrocery', {
-			url: '/shop-groceries/:shopGroceryId',
-			templateUrl: 'modules/shop-groceries/views/view-shop-grocery.client.view.html'
-		}).
-		state('editShopGrocery', {
-			url: '/shop-groceries/:shopGroceryId/edit',
-			templateUrl: 'modules/shop-groceries/views/edit-shop-grocery.client.view.html'
-		});
-	}
+  function($stateProvider) {
+    // Shop groceries state routing
+    $stateProvider.
+        state('listShopGroceries', {
+          url: '/shop-groceries',
+          templateUrl: 'modules/shop-groceries/views/list-shop-groceries.client.view.html'
+        }).
+        state('listNearShopGroceries', {
+          url: '/shop-near-groceries',
+          templateUrl: 'modules/shop-groceries/views/list-near-shop-groceries.client.view.html'
+        }).
+        state('createShopGrocery', {
+          url: '/shop-groceries/create',
+          templateUrl: 'modules/shop-groceries/views/create-shop-grocery.client.view.html'
+        }).
+        state('viewShopGrocery', {
+          url: '/shop-groceries/:shopGroceryId',
+          templateUrl: 'modules/shop-groceries/views/view-shop-grocery.client.view.html'
+        }).
+        state('editShopGrocery', {
+          url: '/shop-groceries/:shopGroceryId/edit',
+          templateUrl: 'modules/shop-groceries/views/edit-shop-grocery.client.view.html'
+        });
+  }
 ]);
--- public/modules/shop-groceries/controllers/shop-groceries.client.controller.js
+++ public/modules/shop-groceries/controllers/shop-groceries.client.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Shop groceries controller
-angular.module('shop-groceries').controller('ShopGroceriesController', ['$scope', '$stateParams', '$location', 'Authentication', 'ShopGroceries',
-	function($scope, $stateParams, $location, Authentication, ShopGroceries) {
+angular.module('shop-groceries').controller('ShopGroceriesController', ['$scope', '$stateParams', '$location', 'Authentication', 'ShopGroceries','$http',
+	function($scope, $stateParams, $location, Authentication, ShopGroceries,$http) {
 		$scope.authentication = Authentication;
     $scope.openalldays = true;
 		// Create new Shop grocery
@@ -11,11 +11,9 @@ angular.module('shop-groceries').controller('ShopGroceriesController', ['$scope'
 			var shopGrocery = new ShopGroceries ({
 				name: this.name
 			});
-
 			// Redirect after save
 			shopGrocery.$save(function(response) {
 				$location.path('shop-groceries/' + response._id);
-
 				// Clear form fields
 				$scope.name = '';
 			}, function(errorResponse) {
@@ -75,6 +73,54 @@ console.log(shopGrocery);
 		$scope.find = function() {
 			$scope.shopGroceries = ShopGroceries.query();
 		};
+
+    $scope.findNearby = function() {
+      //$scope.shopGroceries = ShopGroceries.query();
+      $scope.pageNumber = 0;
+      console.log($scope.areaLocation);
+      if ($scope.areaLocation) {
+        $scope.areaLng = $scope.areaLocation.geometry.location.lng();
+        $scope.areaLat = $scope.areaLocation.geometry.location.lat();
+      }
+      else{
+        $scope.areaLng = 71;
+        $scope.areaLat = 19;
+      }
+        $scope.pageNumber = 1;
+        console.log($scope.pageNumber);
+        $http.get('http://getmeher.com:3000/shop-groceries/near/' + $scope.areaLng + '/' + $scope.areaLat + '/' + $scope.pageNumber).
+            then(function (response) {
+              $scope.shopGroceries = (response.data);
+              $scope.$broadcast('scroll.infiniteScrollComplete');
+            }, function (response) {
+              // called asynchronously if an error occurs
+              // or server returns response with an error status.
+            });
+    };
+
+    $scope.loadmore = function() {
+      //$scope.shopGroceries = ShopGroceries.query();
+      console.log($scope.areaLocation);
+      if ($scope.areaLocation) {
+        $scope.areaLng = $scope.areaLocation.geometry.location.lng();
+        $scope.areaLat = $scope.areaLocation.geometry.location.lat();
+      }
+      else{
+        $scope.areaLng = 71;
+        $scope.areaLat = 19;
+      }
+      $scope.pageNumber = $scope.pageNumber + 1;
+      console.log($scope.pageNumber);
+      $http.get('http://getmeher.com:3000/shop-groceries/near/' + $scope.areaLng + '/' + $scope.areaLat + '/' + $scope.pageNumber).
+          then(function (response) {
+            $scope.shopGroceries = (response.data);
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+          }, function (response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+          });
+    };
+
 
     $scope.updateSpecific = function(shopGroceryData) {
       var shopGrocery = shopGroceryData;
