87a6e6b5124b9a328f8be82033b34cb70dc8d293
gehlotchirag <gehlotchirag1988@gmail.com>
2015-12-07 17:23:18 +0530

    added files

--- public/modules/shop-groceries/config/shop-groceries.client.routes.js
+++ public/modules/shop-groceries/config/shop-groceries.client.routes.js
@@ -5,25 +5,31 @@ angular.module('shop-groceries').config(['$stateProvider',
   function($stateProvider) {
     // Shop groceries state routing
     $stateProvider.
-        state('listShopGroceries', {
-          url: '/shop-groceries',
-          templateUrl: 'modules/shop-groceries/views/list-shop-groceries.client.view.html'
-        }).
-        state('listNearShopGroceries', {
-          url: '/shop-near-groceries',
-          templateUrl: 'modules/shop-groceries/views/list-near-shop-groceries.client.view.html'
-        }).
-        state('createShopGrocery', {
-          url: '/shop-groceries/create',
-          templateUrl: 'modules/shop-groceries/views/create-shop-grocery.client.view.html'
-        }).
-        state('viewShopGrocery', {
-          url: '/shop-groceries/:shopGroceryId',
-          templateUrl: 'modules/shop-groceries/views/view-shop-grocery.client.view.html'
-        }).
-        state('editShopGrocery', {
-          url: '/shop-groceries/:shopGroceryId/edit',
-          templateUrl: 'modules/shop-groceries/views/edit-shop-grocery.client.view.html'
-        });
+    state('listShopGroceries', {
+      url: '/shop-groceries',
+      templateUrl: 'modules/shop-groceries/views/list-shop-groceries.client.view.html'
+    }).
+    state('listNearShopGroceries', {
+      url: '/shop-near-groceries',
+      templateUrl: 'modules/shop-groceries/views/list-near-shop-groceries.client.view.html'
+    }).
+    state('createShopGrocery', {
+      url: '/shop-groceries/create',
+      templateUrl: 'modules/shop-groceries/views/create-shop-grocery.client.view.html'
+    }).
+    state('viewShopGrocery', {
+      url: '/shop-groceries/:shopGroceryId',
+      templateUrl: 'modules/shop-groceries/views/view-shop-grocery.client.view.html'
+    }).
+
+    state('editfromReminderShopGrocery', {
+      url: '/shop-near-groceries/:shopGroceryId/edit',
+      templateUrl: 'modules/shop-groceries/views/edit-shop-grocery.client.view.html'
+    }).
+
+    state('editShopGrocery', {
+      url: '/shop-groceries/:shopGroceryId/edit',
+      templateUrl: 'modules/shop-groceries/views/edit-shop-grocery.client.view.html'
+    });
   }
 ]);
