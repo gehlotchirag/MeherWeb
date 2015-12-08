c54a182b3360182e3bf660918a3915f69101912b
gehlotchirag <gehlotchirag1988@gmail.com>
2015-12-08 21:04:47 +0530

    added files

--- app/routes/shop-fruits.server.routes.js
+++ app/routes/shop-fruits.server.routes.js
@@ -18,7 +18,7 @@ module.exports = function(app) {
   app.route('/shop-fruits/mobile/:mobile/:deviceId')
       .get(shopFruits.shopFruitByMobile);
 
-  app.route('/shop-fruits/devices/app')
+  app.route('/shop-fruits-devices/')
       .get(shopFruits.listByDevice);
 
   app.route('/shop-fruits/:shopFruitId')
